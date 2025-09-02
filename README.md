4ed20585c4ccbf6aa05578626fb0836baac9eff4
ms358 <ms358@njit.edu>
2025-09-02 11:11:50 -0400

    added some more info on roadmap, experimented with composables

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ gNameDrop lets you share your contact info using NFC to get that fruit phone exp
 - All of the above because I don't know how to do that stuff
 ## Installation
 Download the APK from the [releases]() tab.
+## Roadmap
+- [ ] Access contacts permission to save a shared contact
+- [ ] Prompt user to create a vCard of themself for sharing
+- [ ] NFC sharing to send contact info
+- [ ] Run in background permission
+- [ ] optional overlay permission for fancy gradient effect
+- [ ] QR code sharing over LAN?/ wifidirect?
+- [ ] Bluetooth sharing
+- [ ] UI
--- app/src/main/java/com/example/gnamedrop/MainActivity.kt
+++ app/src/main/java/com/example/gnamedrop/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +22,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             GNameDropTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MessageCard(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+                        MessageCard(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        FilledButtonExample()
+                    }
+
                 }
             }
         }
@@ -38,10 +44,24 @@ fun MessageCard(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun FilledButtonExample(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
+        Text("Share")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun MessageCard() {
     GNameDropTheme {
         MessageCard("Android")
+    }
+}
+@Preview
+@Composable
+fun FilledButtonExample() {
+    GNameDropTheme {
+	FilledButtonExample(onClick = {})
     }
 }
